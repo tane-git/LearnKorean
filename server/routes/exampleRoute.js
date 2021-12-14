4803c37a4a87e2e4b4c949e259a0c9c5609afb9c
tane-git <tanelives@gmail.com>
2021-12-14 21:12:12 +1300

    add comments to clarify one and two way translations

--- server/routes/exampleRoute.js
+++ server/routes/exampleRoute.js
@@ -13,6 +13,7 @@ const translate = new Translate()
 router.post('/', (req, res) => {
   const input = req.body.input
 
+  // Two way translation
   translate.detect(input)
     .then(result => {
       return result[0].language
@@ -21,12 +22,13 @@ router.post('/', (req, res) => {
       return language === 'en' ? translate.translate(input, 'ko') : translate.translate(input, 'en')
     })
     .then(translation => {
-      console.log('TCL: translation', translation)
       res.send({ translation })
       return null
     })
-    .catch(console.log('google api not working...'))
+    // .catch(console.log('google api not working...'))
+    .catch(err => console.error(err))
 
+  // One way translation
   // translate.translate(input, 'ko')
   //   .then(translation => {
   //     console.log('TCL: translation', translation)
