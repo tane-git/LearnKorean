0fbe6e18e517dd311c603fe4283e03b9e96145bb
tane-git <tanelives@gmail.com>
2021-12-14 18:25:16 +1300

    clean code base now that google api works

--- server/routes/exampleRoute.js
+++ server/routes/exampleRoute.js
@@ -4,42 +4,12 @@ const router = express.Router()
 
 module.exports = router
 
-// router.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-const projectId = 'YOUR_PROJECT_ID'
-
 // Imports the Google Cloud client library
 const { Translate } = require('@google-cloud/translate').v2
 
 // Instantiates a client
-const translate = new Translate({ projectId })
-
-async function quickStart () {
-  // The text to translate
-  const text = 'Hello, world!'
-
-  // The target language
-  const target = 'ko'
-
-  const [translation] = await translate.translate(text, target)
-  // console.log(`Text: ${text}`)
-  // console.log(`Translation: ${translation}`)
-
-  return translation
-}
-
-// router.get('/', (req, res) => {
-//   quickStart()
-//     .then(translation => {
-//       res.send(translation)
-//       return null
-//     })
-//     .catch(err => {
-//       console.error('ERROR:', err)
-//     })
-// })
+const translate = new Translate()
+console.log('translate client: ', translate)
 
 router.get('/', (req, res) => {
   translate.translate('hello', 'ko')
